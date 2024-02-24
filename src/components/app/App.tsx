7255b183d1660aa1a761564150ca708338ee9339
vertsyg <rmdlfg601@mail.ru>
2024-02-24 15:11:24 +0300

    add API request

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import { data } from '../../utils/data';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import styles from './app.module.css'
+import { fetchData } from '../../utils/data';
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const responseData = await fetchData();
+        setData(responseData);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className={styles.app}>
-      <AppHeader/>
+      <AppHeader />
       <main>
-        <BurgerIngredients items={data}/>
-        <BurgerConstructor items={data}/>
+        {loading ? (
+          <p>Загрузка</p>
+        ) : error ? (
+          <p>Кажется сегодня на пп</p>
+        ) : (
+          <>
+            <BurgerIngredients items={data} />
+            <BurgerConstructor items={data} />
+          </>
+        )}
       </main>
     </div>
   );
